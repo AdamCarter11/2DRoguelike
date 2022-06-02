b1fdc473f54a15abe54b139dfb7aa69a2ce56c1b
Adam <57573881+AdamCarter11@users.noreply.github.com>
2022-06-02 13:05:51 -0700

    test

---
+++ Roguelike_SummerProject/Assets/Scripts/SimpleRandoWalkGen.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRandoWalkGen : MonoBehaviour
+{
+    protected Vector2Int startPos = Vector2Int.zero;
+}
